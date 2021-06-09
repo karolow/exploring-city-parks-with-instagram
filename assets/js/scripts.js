b8215f92d0eac8a9a4799e43fa959d67e46f1bed
karolow <karol.piekarski@gmail.com>
2021-06-09 12:01:44 +0200

    fix mobile menu to handle internal #h1 links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,19 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+var menuLinks = document.querySelectorAll('#main-menu-mobile a');
+
 
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+menuLinks.forEach(function(link) {
+  link.onclick = function() {
+    menuContainer.classList.remove('open');
+    menuTrigger.classList.remove('is-active');
+    body.classList.remove('lock-scroll');
+  }
+})
